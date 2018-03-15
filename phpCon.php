29939712779cac17eb13b048587935508fb39c5c
EC2 Default User <ec2-user@ip-172-31-6-39.ca-central-1.compute.internal>
2018-03-15 22:26:54 +0000

    Trying to make SQL and PHP work.

--- phpCon.php
+++ phpCon.php
@@ -2,24 +2,22 @@
 $servername = "MSCI_444@ec2-35-182-240-106.ca-central-1.compute.amazonaws.com";
 $un = "bot";
 $p = "imadumbbot";
-echo "<p> step 1 </p>";
+
 $con = new mysqli($severname, $un, $p);
 
 if(!$con){
-	echo "<p> Connection Failed </p>";
+	echo "Connection Failed";
 	die("connection Failed:" . mysqli_connect_error());
 }
-echo "<p>Connection SUccessful</p>";
-echo "</body>";
 
-$sql = "Select Last_name, First_name, FROM Student_test Where First_name = Jason";
+$sql = "Select Last_name, First_name FROM Student_test Where First_name = "Jason";
 $result = $conn -> query($sql);
-echo $result;
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "$row["First_name"]. $row["Last_name"];
+	echo "test";
+        echo "Name: " . $row["First_name"] . " " .  $row["Last_name"];
     }
 } else {
     echo "0 results";
