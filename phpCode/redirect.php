8d38b898dd5b2e2d344720d7cea6080f53dc83e4
EC2 Default User <ec2-user@ip-172-31-6-39.ca-central-1.compute.internal>
2018-03-22 00:12:51 +0000

    added query code to redirect page

--- phpCode/redirect.php
+++ phpCode/redirect.php
@@ -1,4 +1,23 @@
 <?php
+	$role="Student";
+	echo $_GET["firstName"];
+	$name=$_GET["firstName"] . " " . $_GET["lastName"];
+	echo "<p>".$name."</p>";
+	if(isset($_GET["teacher"])){
+		$role="Teacher";
+	}
+	$sql="Select count(*) from " . $role . " where " . $role . ".`Student Name` = " . $name;
+	$result=mysqli_query($con,$sql);
+	$count=mysqli_fetch_field($result);
+	if($count>0){
+echo "<p>result > 0 </p>";
+		$_SESSION["failed"]=0;
+		$_SESSION['name']=$name;
+		$_SESSION['role']=$role;
+	}else{
+echo "<p> no result :( </p>";
+		$_SESSION['failed']=1;
+	}
 	if($_SESSION['failure']==1){
 		header("Location: http://35.182.240.106/tutoring-for-students/phpCode/login.php");
 		die();
