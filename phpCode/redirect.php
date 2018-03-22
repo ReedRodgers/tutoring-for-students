75af1ed3157323621609ad1b524dc8d9ded59d43
EC2 Default User <ec2-user@ip-172-31-6-39.ca-central-1.compute.internal>
2018-03-22 00:28:36 +0000

    Altered radio button values. Issue #5

--- phpCode/redirect.php
+++ phpCode/redirect.php
@@ -3,27 +3,29 @@
 	echo $_GET["firstName"];
 	$name=$_GET["firstName"] . " " . $_GET["lastName"];
 	echo "<p>".$name."</p>";
-	if(isset($_GET["teacher"])){
-		$role="Teacher";
-	}
+	$role=$_GET["student-teacher"]
 	$sql="Select count(*) from " . $role . " where " . $role . ".`Student Name` = " . $name;
 	$result=mysqli_query($con,$sql);
 	$count=mysqli_fetch_field($result);
 	if($count>0){
 echo "<p>result > 0 </p>";
 		$_SESSION["failed"]=0;
-		$_SESSION['name']=$name;
-		$_SESSION['role']=$role;
+		$_SESSION["name"]=$name;
+		$_SESSION["role"]=$role;
 	}else{
 echo "<p> no result :( </p>";
-		$_SESSION['failed']=1;
+		$_SESSION["failed"]=1;
 	}
-	if($_SESSION['failure']==1){
-		header("Location: http://35.182.240.106/tutoring-for-students/phpCode/login.php");
-		die();
+	if($_SESSION["failure"]==1){
+		echo "<p>".$_SESSION["name"]."</p>".
+		"<form action='http://35.182.240.106/tutoring-for-students/phpCode/login.php'>".
+			"<input type='submit'>OK</input>".
+		"</form>";
 	}elseif($_SESSION['role']=="Student"){
-		header("Location: http://35.182.240.106/tutoring-for-students/phpCode/student_homepage.php");
-		die();
+		echo "<p>".$_SESSION["name"]."</p>".
+		"<form action='http://35.182.240.106/tutoring-for-students/phpCode/student_homepage.php'>".
+			"<input type='submit'>OK</input>".
+		"</form>";
 	}else{
 		header("Location: http://35.182.240.106/tutoring-for-students/phpCode/teacher_homepage.php");
 		die();
